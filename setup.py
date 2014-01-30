4871f9b53c234171f4d97298655e40f4f89dc1bb
Evan Todd <et1337@gmail.com>
2014-01-30 14:08:54 -0500

    Add dimensions dependency to setup script

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
 	author_email = 'support@sidebolt.com',
 	scripts = ['bin/ccbup.py'],
 	url = 'http://sidebolt.com/',
+	install_requires = ['dimensions==0.0.2'],
 	description = 'Converts CocosBuilder 3 files to the SpriteBuilder 1.0 format.'
 )
